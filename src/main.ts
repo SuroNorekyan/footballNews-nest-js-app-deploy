bbda8d1541ad3bb093878eb0670518a1bab27e84
SuroNorekyan <suren.norekyan123@gmail.com>
2023-08-29 23:37:34 +0400

    process env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(port);
 }
 bootstrap();
